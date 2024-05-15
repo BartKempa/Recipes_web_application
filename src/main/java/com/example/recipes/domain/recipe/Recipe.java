184a4c51238c2d11907cdbb1ccbb31e08d0043ce
BartKempa <bartekkempa@gmail.com>
2024-05-15 13:09:41 +0200

    dodano mapera do RecpieDto

--- src/main/java/com/example/recipes/domain/recipe/Recipe.java
+++ src/main/java/com/example/recipes/domain/recipe/Recipe.java
@@ -15,7 +15,27 @@ public class Recipe {
     @JoinColumn(name = "type_id", referencedColumnName = "id")
     private Type type;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
 }
--- src/main/java/com/example/recipes/domain/recipe/RecipeDtoMapper.java
+++ src/main/java/com/example/recipes/domain/recipe/RecipeDtoMapper.java
@@ -1,4 +1,16 @@
 package com.example.recipes.domain.recipe;
 
+import com.example.recipes.domain.recipe.dto.RecipeDto;
+
 public class RecipeDtoMapper {
+
+    static RecipeDto map(Recipe recipe){
+        return new RecipeDto(
+                recipe.getId(),
+                recipe.getName(),
+                recipe.getType().getName()
+        );
+    }
+
+
 }
