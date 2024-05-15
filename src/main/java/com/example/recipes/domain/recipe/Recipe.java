ebcae44f8b604839f8731a5f8266026ca573e3e6
BartKempa <bartekkempa@gmail.com>
2024-05-15 10:23:01 +0200

    encje recipe i type

--- src/main/java/com/example/recipes/domain/recipe/Recipe.java
+++ src/main/java/com/example/recipes/domain/recipe/Recipe.java
@@ -1,4 +1,21 @@
 package com.example.recipes.domain.recipe;
 
+import com.example.recipes.domain.type.Type;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "type_id", referencedColumnName = "id")
+    private Type type;
+
+
+
+
 }
