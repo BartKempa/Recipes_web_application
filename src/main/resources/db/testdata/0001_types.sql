e4b4719f25e60d58f60e3dde855d57461f39a944
BartKempa <bartekkempa@gmail.com>
2024-05-15 12:51:05 +0200

    dodano dane testowe dla recpie i type

---
+++ src/main/resources/db/testdata/0001_types.sql
@@ -0,0 +1,8 @@
+insert into type (name) values ('Mięsa');
+insert into type (name) values ('Ryby');
+insert into type (name) values ('Desery');
+insert into type (name) values ('Napoje');
+insert into type (name) values ('Sałatki');
+insert into type (name) values ('Zupy');
+insert into type (name) values ('Śniadania');
+insert into type (name) values ('Przekąski');
