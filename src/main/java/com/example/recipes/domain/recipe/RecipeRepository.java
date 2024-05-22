285903ecefdc5c13091bcd7bfc3dbbd8395fd102
BartKempa <bartekkempa@gmail.com>
2024-05-22 09:36:03 +0200

    metoda  findAllByType_NameIgnoreCase w RecipeRepository

--- src/main/java/com/example/recipes/domain/recipe/RecipeRepository.java
+++ src/main/java/com/example/recipes/domain/recipe/RecipeRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-
-
+    List<Recipe> findAllByType_NameIgnoreCase(String name);
 
 }
