0bab8a62ca643eceb2b4b4bb5137d0ac8c71551b
BartKempa <bartekkempa@gmail.com>
2024-05-27 11:03:33 +0200

    metoda prepareStorageDirectories

--- src/main/java/com/example/recipes/storage/FileStorageService.java
+++ src/main/java/com/example/recipes/storage/FileStorageService.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Service
@@ -15,10 +19,21 @@ public class FileStorageService {
     public FileStorageService(@Value("${app.storage.location") String storageLocation) {
         this.imageStorageLocation = storageLocation + "/img/";
         Path imageStoragePath = Path.of(this.imageStorageLocation);
-        prepareStorageDirectories(imageStorageLocation);
+        prepareStorageDirectories(imageStoragePath);
     }
 
-    private void prepareStorageDirectories(String imageStorageLocation) {
+    private void prepareStorageDirectories(Path imageStoragePath) {
+        try {
+            if (Files.notExists(imageStoragePath)){
+                Files.createDirectories(imageStoragePath);
+                logger.info("Storage directory for images created %S".formatted(imageStoragePath.toAbsolutePath().toString()));
+            }
+        } catch (IOException e){
+            throw new UncheckedIOException("Creation of image storage failed", e);
+        }
+
+
+
 
     }
 }
