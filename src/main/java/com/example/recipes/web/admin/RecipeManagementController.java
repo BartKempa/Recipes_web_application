c9ad1a0b32caf686cbc0cb20e68543df9454441d
BartKempa <bartekkempa@gmail.com>
2024-05-28 09:19:18 +0200

    dodano addRecipe w RecipeManagementController

--- src/main/java/com/example/recipes/web/admin/RecipeManagementController.java
+++ src/main/java/com/example/recipes/web/admin/RecipeManagementController.java
@@ -6,6 +6,8 @@ import com.example.recipes.domain.type.TypeService;
 import com.example.recipes.domain.type.dto.TypeDto;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -25,6 +27,16 @@ public class RecipeManagementController {
         List<TypeDto> types = typeService.findAllTypes();
         model.addAttribute("types", types);
         return "admin/recipe-form";
+    }
+
+    @PostMapping("/admin/dodaj-przepis")
+    public String addRecipe(RecipeSaveDto recipe, RedirectAttributes redirectAttributes){
+        recipeService.addRecipe(recipe);
+        redirectAttributes.addFlashAttribute(
+                AdminController.NOTIFICATION_ATTRIBUTE,
+                "Przepis %s został zapisany".formatted(recipe.getName())
+                );
+        return "redirect:/admin";
     }
 
 
