b29e0578b516007f4257e1c42f389aa9629c458b
BartKempa <bartekkempa@gmail.com>
2024-05-30 16:20:19 +0200

    dodao loadUserByUsername w CustomUserDetailsService

---
+++ src/main/java/com/example/recipes/domain/config/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.example.recipes.domain.config.security;
+
+import com.example.recipes.domain.user.UserService;
+import com.example.recipes.domain.user.dto.UserCredentialsDto;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userService.findCredentialsByEmail(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email %s not found".formatted(username)));
+    }
+
+    private UserDetails createUserDetails(UserCredentialsDto credentials) {
+        return User.builder()
+                .username(credentials.getEmail())
+                .password(credentials.getPassword())
+                .roles(credentials.getRoles().toArray(String[]::new))
+                .build();
+    }
+}
