c273e08400fe159895490a1699959d8ba1861f4b
BartKempa <bartekkempa@gmail.com>
2024-06-04 09:00:41 +0200

    dodano metodę do rejestracji użytkownika

--- src/main/java/com/example/recipes/domain/user/UserService.java
+++ src/main/java/com/example/recipes/domain/user/UserService.java
@@ -1,20 +1,39 @@
 package com.example.recipes.domain.user;
 
 import com.example.recipes.domain.user.dto.UserCredentialsDto;
+import com.example.recipes.domain.user.dto.UserRegistrationDto;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 public class UserService {
+    private static final String DEFAULT_USER_ROLE = "USER";
     private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserRoleRepository userRoleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.userRoleRepository = userRoleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Optional<UserCredentialsDto> findCredentialsByEmail(String email){
         return userRepository.findByEmail(email)
                 .map(UserCredentialsDtoMapper::map);
     }
+
+    @Transactional
+    public void registerUserWithDefaultRole(UserRegistrationDto userRegistrationDto){
+        User user = new User();
+        user.setEmail(userRegistrationDto.getEmail());
+        user.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
+        UserRole userRole = userRoleRepository.findByName(DEFAULT_USER_ROLE).orElseThrow();
+        user.getRoles().add(userRole);
+        userRepository.save(user);
+    }
+
 }
