e1ac00db87ff3a9e09b12f416c6fb0eda6bad353
BartKempa <bartekkempa@gmail.com>
2024-06-05 12:40:52 +0200

    dodano metodę addOrUpdateRating do RatingService

---
+++ src/main/java/com/example/recipes/domain/rating/RatingService.java
@@ -0,0 +1,32 @@
+package com.example.recipes.domain.rating;
+
+import com.example.recipes.domain.recipe.Recipe;
+import com.example.recipes.domain.recipe.RecipeRepository;
+import com.example.recipes.domain.user.User;
+import com.example.recipes.domain.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RatingService {
+
+    private final RatingRepository ratingRepository;
+    private final RecipeRepository recipeRepository;
+    private final UserRepository userRepository;
+
+    public RatingService(RatingRepository ratingRepository, RecipeRepository recipeRepository, UserRepository userRepository) {
+        this.ratingRepository = ratingRepository;
+        this.recipeRepository = recipeRepository;
+        this.userRepository = userRepository;
+    }
+
+    public void addOrUpdateRating(String userEmail, long recipeId, int rating){
+        Rating ratingToSaveOrUpdate = ratingRepository.findByUser_EmailAndRecipe_Id(userEmail, recipeId).orElseGet(Rating::new);
+        User user = userRepository.findByEmail(userEmail).orElseThrow();
+        Recipe recipe = recipeRepository.findById(recipeId).orElseThrow();
+        ratingToSaveOrUpdate.setUser(user);
+        ratingToSaveOrUpdate.setRecipe(recipe);
+        ratingToSaveOrUpdate.setRating(rating);
+        ratingRepository.save(ratingToSaveOrUpdate);
+    }
+
+}
