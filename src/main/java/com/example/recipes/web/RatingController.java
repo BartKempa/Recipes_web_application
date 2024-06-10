45803e5f34afc787fdc26f7feb0aa3e7569ee085
BartKempa <bartekkempa@gmail.com>
2024-06-10 13:15:38 +0200

    dodano addRecipeRating

--- src/main/java/com/example/recipes/web/RatingController.java
+++ src/main/java/com/example/recipes/web/RatingController.java
@@ -1,7 +1,11 @@
 package com.example.recipes.web;
 
 import com.example.recipes.domain.rating.RatingService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RatingController {
@@ -9,5 +13,15 @@ public class RatingController {
 
     public RatingController(RatingService ratingService) {
         this.ratingService = ratingService;
+    }
+
+    @PostMapping("/ocen-przepis")
+    public String addRecipeRating(@RequestParam long recipeId,
+                                  @RequestParam int rating,
+                                  @RequestHeader String referer,
+                                  Authentication authentication){
+        String currentUserEmail = authentication.getName();
+        ratingService.addOrUpdateRating(currentUserEmail, recipeId, rating);
+        return "redirect:" + referer;
     }
 }
