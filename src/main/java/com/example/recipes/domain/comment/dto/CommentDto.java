cc9d767fc8c289bd8374c72568ca06db5dd2e89f
BartKempa <bartekkempa@gmail.com>
2024-06-18 10:35:27 +0200

    dodano interfejs CommentRepository oraz metodę findAll

--- src/main/java/com/example/recipes/domain/comment/dto/CommentDto.java
+++ src/main/java/com/example/recipes/domain/comment/dto/CommentDto.java
@@ -1,4 +1,16 @@
 package com.example.recipes.domain.comment.dto;
 
+import com.example.recipes.domain.recipe.Recipe;
+import com.example.recipes.domain.user.User;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+
 public class CommentDto {
+    private Long id;
+    private boolean approved;
+    private String text;
+    private long recipeId;
+    private String userName;
 }
