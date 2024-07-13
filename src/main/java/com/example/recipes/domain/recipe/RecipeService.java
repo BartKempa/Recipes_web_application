be709d71d987962bb4afbec4275546f15fb09120
BartKempa <bartekkempa@gmail.com>
2024-07-13 12:38:10 +0200

    :dodano metodę findRatedRecipesForUser do srevisu

--- src/main/java/com/example/recipes/domain/recipe/RecipeService.java
+++ src/main/java/com/example/recipes/domain/recipe/RecipeService.java
@@ -89,4 +89,10 @@ public class RecipeService {
                 .map(RecipeDtoMapper::mapMainInfo);
     }
 
+    public Page<RecipeMainInfoDto> findRatedRecipesForUser(String email, int pageNumber, int pageSize, String sortField) {
+        Sort sort = Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, sort);
+        return recipeRepository.findAllRatedRecipesByUser(email, pageable)
+                .map(RecipeDtoMapper::mapMainInfo);
+    }
 }
