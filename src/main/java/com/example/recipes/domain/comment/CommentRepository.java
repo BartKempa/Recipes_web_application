49f4c578d65f9ee35937c1bff081697e10948d14
BartKempa <bartekkempa@gmail.com>
2024-07-22 13:48:29 +0200

    dodano metodę findPaginatedCommentsList do CommentService

--- src/main/java/com/example/recipes/domain/comment/CommentRepository.java
+++ src/main/java/com/example/recipes/domain/comment/CommentRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CommentRepository extends CrudRepository<Comment, Long> {
+public interface CommentRepository extends CrudRepository<Comment, Long>, PagingAndSortingRepository<Comment, Long> {
     List<Comment> findAllByRecipeId(long recipeId);
 
     @Query("SELECT c FROM Comment c JOIN c.user u WHERE u.email =:email")
--- src/main/java/com/example/recipes/domain/comment/CommentService.java
+++ src/main/java/com/example/recipes/domain/comment/CommentService.java
@@ -64,4 +64,12 @@ public class CommentService {
         commentRepository.delete(comment);
     }
 
+    public Page<CommentDto> findPaginatedCommentsList(int pageNumber, int pageSize, String sortField, String sortDirection){
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNumber -1, pageSize, sort);
+        return commentRepository.findAll(pageable)
+                .map(CommentDtoMapper::map);
+
+    }
+
 }
