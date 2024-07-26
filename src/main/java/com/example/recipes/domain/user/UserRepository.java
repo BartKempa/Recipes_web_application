5bcddf151fe9ed58bf8eeb39ddcd430da9af0881
BartKempa <bartekkempa@gmail.com>
2024-07-26 13:26:43 +0200

    dodano metodę do wyszukiwania listy użytkowników w service

--- src/main/java/com/example/recipes/domain/user/UserRepository.java
+++ src/main/java/com/example/recipes/domain/user/UserRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long>, PagingAndSortingRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     @Query("SELECT COUNT (u) FROM User u JOIN u.favoriteRecipes r WHERE r.id = :recipeId")
--- src/main/java/com/example/recipes/domain/user/UserService.java
+++ src/main/java/com/example/recipes/domain/user/UserService.java
@@ -9,6 +9,10 @@ import com.example.recipes.domain.recipe.Recipe;
 import com.example.recipes.domain.recipe.RecipeRepository;
 import com.example.recipes.domain.user.dto.UserCredentialsDto;
 import com.example.recipes.domain.user.dto.UserRegistrationDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -121,6 +125,13 @@ public class UserService {
         comment.setText(commentDto.getText());
         comment.setApproved(false);
         commentRepository.save(comment);
+    }
+
+    public Page<UserRegistrationDto> getAllUsers(int pageNumber, int pageSize, String sortField, String sortDirection){
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNumber-1, pageSize, sort);
+        return userRepository.findAll(pageable)
+                .map(UserRegistrationDtoMapper::map);
     }
 }
 
