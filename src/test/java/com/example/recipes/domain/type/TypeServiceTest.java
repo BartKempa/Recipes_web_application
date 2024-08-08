dfd25924b57fff5e7fe99f1f7cf3f748f7d3e26e
BartKempa <bartekkempa@gmail.com>
2024-08-08 12:37:16 +0200

    dodano shouldFindAllTypes do testowania findAllTypes

--- src/test/java/com/example/recipes/domain/type/TypeServiceTest.java
+++ src/test/java/com/example/recipes/domain/type/TypeServiceTest.java
@@ -5,13 +5,20 @@ import com.example.recipes.domain.rating.RatingRepository;
 import com.example.recipes.domain.recipe.RecipeRepository;
 import com.example.recipes.domain.type.dto.TypeDto;
 import com.example.recipes.domain.user.UserRepository;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TypeServiceTest {
@@ -63,8 +70,39 @@ class TypeServiceTest {
     }
 
     @Test
-    void findAllTypes() {
+    void shouldFindAllTypes() {
+        //given
+        RecipeRepository recipeRepositoryMock = Mockito.mock(RecipeRepository.class);
+        TypeRepository typeRepositoryMock = Mockito.mock(TypeRepository.class);
+        UserRepository userRepository = Mockito.mock(UserRepository.class);
+        RatingRepository ratingRepositoryMock = Mockito.mock(RatingRepository.class);
+        CommentRepository commentRepositoryMock = Mockito.mock(CommentRepository.class);
+        TypeService typeService = new TypeService(typeRepositoryMock, recipeRepositoryMock, ratingRepositoryMock, commentRepositoryMock, userRepository);
+
+        Type type1 = new Type();
+        type1.setId(1L);
+        type1.setName("Zupa");
+
+        Type type2 = new Type();
+        type2.setId(2L);
+        type2.setName("Pizza");
+
+        List<Type> typeList = new ArrayList<>();
+        typeList.add(type1);
+        typeList.add(type2);
+
+        Mockito.when(typeRepositoryMock.findAll()).thenReturn(typeList);
+
+        //when
+        List<TypeDto> allTypes = typeService.findAllTypes();
+
+        //then
+        MatcherAssert.assertThat(allTypes, hasSize(2));
+        assertThat(allTypes.get(0).getName(), is("Zupa"));
+        assertThat(allTypes.get(1).getName(), is("Pizza"));
     }
+
+
 
     @Test
     void findPaginatedTypesList() {
