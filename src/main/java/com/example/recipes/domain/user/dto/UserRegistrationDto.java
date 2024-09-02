74bda508df899be8b4f04c8b9b8ec525c6de4de3
BartKempa <bartekkempa@gmail.com>
2024-09-02 09:56:14 +0200

    dodano walidację do klasy UserRegistrationDto

--- src/main/java/com/example/recipes/domain/user/dto/UserRegistrationDto.java
+++ src/main/java/com/example/recipes/domain/user/dto/UserRegistrationDto.java
@@ -1,14 +1,30 @@
 package com.example.recipes.domain.user.dto;
 
+import com.example.recipes.domain.user.validation.UniqueEmail;
+import jakarta.validation.constraints.*;
+
 import java.util.Set;
 
 public class UserRegistrationDto {
     private Long id;
+    @NotBlank
+    @Email
+    @UniqueEmail
     private String email;
+    @NotBlank
+    @Size(min = 5, max = 200)
     private String password;
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String firstName;
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String lastName;
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String nickName;
+    @Min(15)
+    @NotNull
     private int age;
 
     public UserRegistrationDto(Long id, String email, String password, String firstName, String lastName, String nickName, int age) {
