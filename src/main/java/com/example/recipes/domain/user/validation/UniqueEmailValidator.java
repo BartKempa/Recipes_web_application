df029ab78cc3d6153b8e856efae4949e1f83ecd4
BartKempa <bartekkempa@gmail.com>
2024-09-02 09:47:45 +0200

    dodano klasę UniqueEmailValidator z metodą isValid

---
+++ src/main/java/com/example/recipes/domain/user/validation/UniqueEmailValidator.java
@@ -0,0 +1,23 @@
+package com.example.recipes.domain.user.validation;
+
+import com.example.recipes.domain.user.UserRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+    private final UserRepository userRepository;
+
+    public UniqueEmailValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void initialize(UniqueEmail constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return userRepository.findByEmail(email).isEmpty();
+    }
+}
