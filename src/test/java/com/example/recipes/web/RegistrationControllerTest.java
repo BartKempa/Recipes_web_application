e142c88a1683092db09eed5f2c1fc92d4f0555d0
BartKempa <bartekkempa@gmail.com>
2024-09-17 10:43:48 +0200

    dodano test shouldRegisterUserWithCorrectData dla metody register

--- src/test/java/com/example/recipes/web/RegistrationControllerTest.java
+++ src/test/java/com/example/recipes/web/RegistrationControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.recipes.web;
 
+import com.example.recipes.domain.user.UserRepository;
 import com.example.recipes.domain.user.dto.UserRegistrationDto;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -12,8 +13,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.regex.Matcher;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -25,6 +29,9 @@ class RegistrationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void shouldRegisterForm() throws Exception {
         mockMvc.perform(get("/rejestracja")
@@ -32,11 +39,28 @@ class RegistrationControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("registration-form"))
                 .andExpect(model().attributeExists("user"))
-                .andExpect(model().attribute("user", Matchers.instanceOf(UserRegistrationDto.class)))
-;
+                .andExpect(model().attribute("user", Matchers.instanceOf(UserRegistrationDto.class)));
     }
 
     @Test
-    void register() {
+    void shouldRegisterUserWithCorrectData() throws Exception {
+        //given
+        final String userMail = "testUser@mail.com";
+        assertFalse(userRepository.findByEmail(userMail).isPresent());
+
+        //when
+        mockMvc.perform(post("/rejestracja")
+                .param("email", userMail)
+                .param("password", "hardPass123!@#")
+                .param("firstName", "Bart")
+                .param("lastName", "Bartkowski")
+                .param("nickName", "Barti")
+                .param("age", String.valueOf(40))
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+
+        //then
+        assertTrue(userRepository.findByEmail(userMail).isPresent());
     }
 }
