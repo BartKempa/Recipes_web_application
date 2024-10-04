3be8af3b6e14b688c7a3d8b80f4cad8cf618dbc2
BartKempa <bartekkempa@gmail.com>
2024-10-04 08:53:09 +0200

    dodano test shouldGetAddTypeForm dla metody addTypeForm

--- src/test/java/com/example/recipes/web/admin/TypeManagementControllerTest.java
+++ src/test/java/com/example/recipes/web/admin/TypeManagementControllerTest.java
@@ -1,15 +1,23 @@
 package com.example.recipes.web.admin;
 
+import com.example.recipes.domain.type.TypeService;
+import com.example.recipes.domain.type.dto.TypeDto;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,8 +28,18 @@ class TypeManagementControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private TypeService typeService;
+
     @Test
-    void addTypeForm() {
+    @WithMockUser(username = "admin@mail.com", roles = "ADMIN")
+    void shouldGetAddTypeForm() throws Exception {
+        mockMvc.perform(get("/admin/dodaj-typ")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/type-form"))
+                .andExpect(model().attributeExists("type"))
+                .andExpect(model().attribute("type", Matchers.instanceOf(TypeDto.class)));
     }
 
     @Test
